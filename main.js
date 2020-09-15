1ba2604975a448d345cf3f685687d7b8541d7982
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2020-09-15 14:09:07 -0400

    added javascript code and tidied my code in css/html

--- main.js
+++ main.js
@@ -2,62 +2,58 @@ const tracking = ["Lapis", "Papyrus", "Scalpellus"];
 
 const rock = tracking[0];
 const paper = tracking[1];
-const scissors = tracking[2]
+const scissors = tracking[2];
 
 const computerChooses = () => {
   const randomizeGame = Math.floor(Math.random() * tracking.length);
   return tracking[randomizeGame];
-} 
+};
 
 const compareChoices = (playersTurn) => {
   const computersTurn = computerChooses();
-  
-  if (computersTurn === playersTurn) { 
+
+  if (computersTurn === playersTurn) {
     return `Computer and Player both have ${computersTurn}! It's a tie!`;
-    
   } else if (computersTurn === rock) {
     if (playersTurn === paper) {
-      return`The player wins! The computer chose ${computersTurn} and the player chose ${playersTurn}.`
+      return `The player wins! The computer chose ${computersTurn} and the player chose ${playersTurn}.`;
     } else {
-      return`The computer wins! The computer chose ${computersTurn} and the player chose ${playersTurn}.`
+      return `The computer wins! The computer chose ${computersTurn} and the player chose ${playersTurn}.`;
     }
   } else if (computersTurn === paper) {
-     if (playersTurn === scissors) {
-      return`The player wins! The computer chose ${computersTurn} and the player chose ${playersTurn}.`
+    if (playersTurn === scissors) {
+      return `The player wins! The computer chose ${computersTurn} and the player chose ${playersTurn}.`;
     } else {
-      return`The computer wins! The computer chose ${computersTurn} and the player chose ${playersTurn}.`
+      return `The computer wins! The computer chose ${computersTurn} and the player chose ${playersTurn}.`;
     }
   } else if (computersTurn === scissors) {
-     if (playersTurn === rock) {
-      return`The player wins! The computer chose ${computersTurn} and the player chose ${playersTurn}.`
+    if (playersTurn === rock) {
+      return `The player wins! The computer chose ${computersTurn} and the player chose ${playersTurn}.`;
     } else {
-      return`The computer wins! The computer chose ${computersTurn} and the player chose ${playersTurn}.`
+      return `The computer wins! The computer chose ${computersTurn} and the player chose ${playersTurn}.`;
     }
   }
-}
+};
 
-// cleaner keeping outside so it doesnt run each time 
-const resultsNode = document.querySelector("#results"); 
+// cleaner keeping outside so it doesnt run each time
+const resultsNode = document.querySelector("#results");
 
 const showResults = (playersTurn) => {
-  resultsNode.innerText = compareChoices(playersTurn); 
-}
-
+  resultsNode.innerText = compareChoices(playersTurn);
+};
 
 // I decided to make 3 event listeners instead of one function, because I thought it was easier to read.
 const rockNode = document.querySelector("#rock");
-rockNode.addEventListener('click', () => {
+rockNode.addEventListener("click", () => {
   showResults(rock);
 });
 
 const paperNode = document.querySelector("#paper");
-paperNode.addEventListener('click', () => {
+paperNode.addEventListener("click", () => {
   showResults(paper);
 });
 
 const scissorsNode = document.querySelector("#scissors");
-scissorsNode.addEventListener('click', () => {
+scissorsNode.addEventListener("click", () => {
   showResults(scissors);
 });
-
-
